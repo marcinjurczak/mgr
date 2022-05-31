de7e9a968c24dc99df63ec1338ad876be2affe62
marcinjurczak <marcin.jurczak12@gmail.com>
2022-05-31 23:42:55 +0200

    Update bookmarks data

--- react/src/data.js
+++ react/src/data.js
@@ -1,4 +1,3 @@
-// Note: having length != 4 will mess with layout based on how the site is styled
 const bookmarks = [
   { name: "Gmail", url: "https://inbox.google.com" },
   { name: "GitHub", url: "https://github.com" },
